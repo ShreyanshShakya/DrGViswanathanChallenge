e93e97ee6b000bb74ad04f4f4d63b0d4f63c1a4d
Shreyansh Shakya <158161518+ShreyanshShakya@users.noreply.github.com>
2026-08-10 09:57:13 +0530

    Time: 1 ms (86%), Space: 44.2 MB (87.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,15 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backtrack("", 0,0,n,res);
+        StringBuilder sb = new StringBuilder();
+        backtrack(sb, 0,0,n,res);
         return res;
     }
-    private void backtrack(String curr, int open, int close, int n, List<String> res){
-        if(curr.length() == 2*n){
-            res.add(curr);
+    private void backtrack(StringBuilder sb, int open, int close, int n, List<String> res){
+        if(sb.length() == 2*n){
+            res.add(sb.toString());
             return;
         }
-        if(open<n) backtrack(curr + "(", open+1, close, n, res);
-        if(close<open) backtrack(curr+")",open,close+1,n,res);
+        if(open<n){
+            sb.append("(");
+            backtrack(sb,open+1,close,n,res);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close<open) {
+            sb.append(")");
+            backtrack(sb,open, close+1, n, res);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
 }

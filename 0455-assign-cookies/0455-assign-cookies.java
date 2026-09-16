3a72ebb9056d8aa09e5b424e1a0ea798a2a8d696
Shreyansh Shakya <158161518+ShreyanshShakya@users.noreply.github.com>
2026-09-16 13:48:47 +0530

    Time: 130 ms (10.22%), Space: 51.4 MB (74.32%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,14 +2,17 @@ class Solution {
     public int findContentChildren(int[] greed, int[] size) {
         Arrays.sort(size);
         Arrays.sort(greed);
+        int res = 0;
         int g = 0;
-        int s = 0;
-        while(s<size.length && g<greed.length){
-            if(size[s] >= greed[g]){
+        for(int s = 0;s<size.length;s++){
+            if(g>greed.length-1){
+                return res;
+            }
+            if(greed[g]<=size[s]){
+                res++;
                 g++;
             }
-            s++;
         }
-        return g;
+        return res;
     }
 }
